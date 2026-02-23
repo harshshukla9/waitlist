04077f84cdd8c71ee6e3a2a7dc65c59e6cb60c4c
harshshukla9 <harshshukla9792@gmail.com>
2026-02-24 02:07:32 +0530

    build fix

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,9 +1,9 @@
 export const MESSAGE_EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 30; // 30 day
 
-const APP_URL = process.env.NEXT_PUBLIC_URL;
+const rawAppUrl = process.env.NEXT_PUBLIC_URL;
 
-if (!APP_URL) {
+if (!rawAppUrl) {
   throw new Error('NEXT_PUBLIC_URL or NEXT_PUBLIC_VERCEL_URL is not set');
 }
 
-export { APP_URL };
+export const APP_URL: string = rawAppUrl;
